fd106112d5b21483e0646073c285c6e1ecdffcfb
Gilang Gozan <95280563+knyxoree@users.noreply.github.com>
2025-07-20 13:44:11 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,234 +7,229 @@ var win = $(window);
 var ww = win.width();
 var wh = win.height();
 
-$(document).ready(function() {
-
-    // load functions
-    imageBG();
-    grid();
-
+$(document).ready(function () {
+  // load functions
+  imageBG();
+  grid();
 });
 
-win.on('load', function() {
+win.on('load', function () {
+  setTimeout(function () {
+    $('#preloader').addClass('hide');
+  }, 1000);
 
-    setTimeout(function() {
-        $('#preloader').addClass('hide');
-    }, 1000);
-
-    // load functions
-    grid();
-
+  // load functions
+  grid();
 });
 
-win.on('resize', function() {
+win.on('resize', function () {
+  // viewport dimensions
+  ww = win.width();
+  wh = win.height();
 
-    // viewport dimensions
-    ww = win.width();
-    wh = win.height();
-
-    // load functions
-    grid();
-    
-
+  // load functions
+  grid();
 });
-
-
 
 /** SHOW/HIDE HEADER */
 /** ===================== */
 
 function show_hide_header() {
+  var last_scroll = 0;
 
-    var last_scroll = 0;
+  win.on('scroll', function () {
+    if (!$('#about').hasClass('visible')) {
+      var scroll = $(this).scrollTop();
 
-    win.on('scroll', function() {
-        if (!$('#about').hasClass('visible')) {
-            var scroll = $(this).scrollTop();
+      if (scroll > last_scroll) {
+        $('#main-header').addClass('hide');
+      } else {
+        $('#main-header').removeClass('hide');
+      }
 
-            if (scroll > last_scroll) {
-                $('#main-header').addClass('hide');
-            } else {
-                $('#main-header').removeClass('hide');
-            }
-
-            last_scroll = scroll;
-        }
-    });
-
+      last_scroll = scroll;
+    }
+  });
 }
-
-
 
 /** BACKGROUND IMAGES */
 /** ===================== */
 
 function imageBG() {
+  $('.imageBG').each(function () {
+    var image = $(this).data('img');
 
-    $('.imageBG').each(function() {
-        var image = $(this).data('img');
-
-        $(this).css({
-            backgroundImage: 'url(' + image + ')',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-        });
+    $(this).css({
+      backgroundImage: 'url(' + image + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
     });
-
+  });
 }
-
 
 /** GRID */
 /** ===================== */
 
 function grid() {
+  var container = $('.grid');
 
-    var container = $('.grid');
+  for (var i = 0; i < container.length; i++) {
+    var active_container = $(container[i]);
+    var container_width = active_container.width();
 
-    for (var i = 0; i < container.length; i++) {
-        var active_container = $(container[i]);
-        var container_width = active_container.width();
+    var items = active_container.find('.entry');
 
-        var items = active_container.find('.entry');
+    // --- RANDOMIZE items ---
+    items = items.sort(function () {
+      return 0.5 - Math.random();
+    });
+    active_container.empty().append(items);
+    // -----------------------
 
-        var cols = parseInt(active_container.data('cols'), 10);
-        var margin = parseInt(active_container.data('margin'), 10);
-        var height = parseFloat(active_container.data('height'));
-        var double_height = parseFloat(active_container.data('double-height'));
+    var cols = parseInt(active_container.data('cols'), 10);
+    var margin = parseInt(active_container.data('margin'), 10);
+    var height = parseFloat(active_container.data('height'));
+    var double_height = parseFloat(active_container.data('double-height'));
 
-        if (!margin) margin = 0;
-        if (!double_height) double_height = 2;
+    if (!margin) margin = 0;
+    if (!double_height) double_height = 2;
 
-        // set margins to the container
-        active_container.css('margin', -Math.floor(margin / 2) + 'px');
+    // set margins to the container
+    active_container.css('margin', -Math.floor(margin / 2) + 'px');
 
-        if (ww >= 1000) {
-            if (!cols) cols = 3;
-        } else if (ww >= 700) {
-            if (cols !== 1) cols = 2;
-        } else {
-            cols = 1;
-        }
+    if (ww >= 1000) {
+      if (!cols) cols = 3;
+    } else if (ww >= 700) {
+      if (cols !== 1) cols = 2;
+    } else {
+      cols = 1;
+    }
 
-        var items_width = Math.floor((container_width / cols) - margin);
-        var items_height = Math.floor(items_width * height);
-        var items_double_height = items_height * double_height;
-        var items_margin = Math.floor(margin / 2);
+    var items_width = Math.floor(container_width / cols - margin);
+    var items_height = Math.floor(items_width * height);
+    var items_double_height = items_height * double_height;
+    var items_margin = Math.floor(margin / 2);
 
-        items.each(function() {
-            $(this).css('width', items_width + 'px');
-            $(this).css('height', items_height + 'px');
-            $(this).css('margin', items_margin + 'px');
+    items.each(function () {
+      $(this).css('width', items_width + 'px');
+      $(this).css('height', items_height + 'px');
+      $(this).css('margin', items_margin + 'px');
 
-            if (!height) $(this).css('height', 'auto');
-            if ($(this).hasClass('w2') && ww >= 500) $(this).css('width', (items_width * 2) + (items_margin * 2) + 'px');  /* Add w2 or h2 to the portfolio item for varoius layout sizes */
-            if ($(this).hasClass('h2') && ww >= 500) $(this).css('height', items_double_height + (items_margin * 2) + 'px');
-        });
+      if (!height) $(this).css('height', 'auto');
+      if ($(this).hasClass('w2') && ww >= 500)
+        $(this).css(
+          'width',
+          items_width * 2 + items_margin * 2 + 'px'
+        );
+      if ($(this).hasClass('h2') && ww >= 500)
+        $(this).css(
+          'height',
+          items_double_height + items_margin * 2 + 'px'
+        );
+    });
 
-        // isotope
-        active_container.isotope({
-            itemSelector: '.entry',
-            transitionDuration: '.2s',
-            hiddenStyle: {
-                opacity: 0
-            },
-            visibleStyle: {
-                opacity: 1
-            },
-            masonry: {
-                columnWidth: items_width + margin
-                
-            }
-        });
+    // isotope
+    active_container.isotope({
+      itemSelector: '.entry',
+      transitionDuration: '.2s',
+      hiddenStyle: {
+        opacity: 0
+      },
+      visibleStyle: {
+        opacity: 1
+      },
+      masonry: {
+        columnWidth: items_width + margin
+      }
+    });
 
-        $('#filters li a').on('click', function(e) {
-            e.preventDefault();
+    $('#filters li a').on('click', function (e) {
+      e.preventDefault();
 
-            var filter = $(this).attr('href');
+      var filter = $(this).attr('href');
 
-            $('#filters li a').removeClass('active');
-            $(this).addClass('active');
+      $('#filters li a').removeClass('active');
+      $(this).addClass('active');
 
-            active_container.isotope({
-                filter: filter
-            });
-        });
-    };
-
+      active_container.isotope({
+        filter: filter
+      });
+    });
+  }
 }
 
 document.getElementById('tahun').textContent = new Date().getFullYear();
 
+const backToTopBtn = document.getElementById('backToTop');
 
-  const backToTopBtn = document.getElementById("backToTop");
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-      backToTopBtn.classList.add("show");
-    } else {
-      backToTopBtn.classList.remove("show");
-    }
-  });
-
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-
-      // Ambil jam saat ini dari waktu lokal pengguna
-    const sekarang = new Date();
-    const jam = sekarang.getHours(); // hasilnya 0–23
-
-    let hasil = "";
-
-    if (jam >= 5 && jam < 10) {
-      hasil = "Have a good day.";
-    } else if (jam >= 10 && jam < 18) {
-      hasil = "Fight on!";
-    } else if (jam >= 18 && jam < 20) {
-      hasil = "Are you feeling tired today?";
-    } else {
-      hasil = "How did your day go? Wishing you an even brighter tomorrow.";
-    }
-
-    document.getElementById("waktu-salam").textContent = hasil;
-
-
-    // Ini script di bagian skill 
-
-  const circles = document.querySelectorAll('.circle');
-  const cards = document.querySelectorAll('.skill-card');
-  let activeCategory = null;
-
-  function showAllCards() {
-    cards.forEach(card => card.style.display = 'block');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
   }
+});
 
-  circles.forEach(circle => {
-    circle.addEventListener('click', () => {
-      const selectedCategory = [...circle.classList].find(c => c !== 'circle');
-
-      if (selectedCategory === 'show-all') {
-        showAllCards();
-        activeCategory = null;
-        return;
-      }
-
-      if (activeCategory === selectedCategory) {
-        showAllCards();
-        activeCategory = null;
-      } else {
-        cards.forEach(card => {
-          if (card.classList.contains(selectedCategory)) {
-            card.style.display = 'block';
-          } else {
-            card.style.display = 'none';
-          }
-        });
-        activeCategory = selectedCategory;
-      }
-    });
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+});
 
-  window.addEventListener('DOMContentLoaded', showAllCards);
+// Greeting based on time
+const sekarang = new Date();
+const jam = sekarang.getHours();
+
+let hasil = '';
+
+if (jam >= 5 && jam < 10) {
+  hasil = 'Have a good day.';
+} else if (jam >= 10 && jam < 18) {
+  hasil = 'Fight on!';
+} else if (jam >= 18 && jam < 20) {
+  hasil = 'Are you feeling tired today?';
+} else {
+  hasil = 'How did your day go? Wishing you an even brighter tomorrow.';
+}
+
+document.getElementById('waktu-salam').textContent = hasil;
+
+// Script bagian skill
+const circles = document.querySelectorAll('.circle');
+const cards = document.querySelectorAll('.skill-card');
+let activeCategory = null;
+
+function showAllCards() {
+  cards.forEach((card) => (card.style.display = 'block'));
+}
+
+circles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    const selectedCategory = [...circle.classList].find(
+      (c) => c !== 'circle'
+    );
+
+    if (selectedCategory === 'show-all') {
+      showAllCards();
+      activeCategory = null;
+      return;
+    }
+
+    if (activeCategory === selectedCategory) {
+      showAllCards();
+      activeCategory = null;
+    } else {
+      cards.forEach((card) => {
+        if (card.classList.contains(selectedCategory)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+      activeCategory = selectedCategory;
+    }
+  });
+});
+
+window.addEventListener('DOMContentLoaded', showAllCards);
